660fc8d49b195eac489fc206fd2c76eebb4731d3
shinbe69 <37283758+shinbe69@users.noreply.github.com>
2020-10-19 11:53:31 +0700

    Add files via upload

--- src/api/index.js
+++ src/api/index.js
@@ -5,11 +5,19 @@ const router = new Router();
 
 const user = require('./user');
 const session = require('./session');
+const createJob = require('./createJob');
+// const viewJob = require('./viewJob');
+// const updateJob = require('./updateJob');
+// const deleteJob = require('./deleteJob');
 
 router.use('/api/health', (request, response) => {
-  response.status(200).json();
+  response.status(400).json();
 });
 router.use('/api/users', user);
 router.use('/api/sessions', session);
+router.use('/api/createJob', createJob);
+// router.use('/api/updateJob', updateJob);
+// router.use('/api/deleteJob', deleteJob);
+// router.use('/api/viewJob', viewJob);
 
 module.exports = router;
