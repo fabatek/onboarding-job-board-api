f2fc0dc608d1e06fef0d9b4e4873698d407d6767
shinbe69 <rantarou@live.com>
2020-10-23 11:27:39 +0700

    Fix code convention

--- src/persistence/job.js
+++ src/persistence/job.js
@@ -3,14 +3,15 @@ const {v4: uuidv4} = require('uuid');
 const db = require('./db');
 
 module.exports = {
-  // Method create() will get all information about the job without id and create a new job with those data
-  async create(para) {
+  async create(jobParameters) {
     try {
       const {rows} = await db.query(sql`
       INSERT INTO jobs
-        VALUES (${uuidv4()}, ${para.title}, ${para.salaryrange}, ${
-        para.description
-      }, current_date, ${para.tags}, ${para.company}, ${para.logoURL})
+        VALUES (${uuidv4()}, ${jobParameters.title}, ${
+        jobParameters.salaryRange
+      }, ${jobParameters.description}, current_date, ${jobParameters.tags}, ${
+        jobParameters.company
+      }, ${jobParameters.logoURL})
         RETURNING id, title, tags;
       `);
       return rows[0];
@@ -22,7 +23,7 @@ module.exports = {
       throw error;
     }
   },
-  // Method find() will get id of the job and send back the job if it exist
+
   async find(id) {
     const {rows} = await db.query(sql`
     SELECT * FROM jobs WHERE id=${id} LIMIT 1;
--- test/job.js
+++ test/job.js
@@ -8,9 +8,9 @@ const {app} = require('../server');
 
 chai.use(chaiHttp);
 chai.should();
-// Test Login using POST method
-describe('/POST post new job', () => {
-  it('POST /api/job', (done) => {
+describe('/POST Post a new job', () => {
+  let accessToken = '';
+  before(function (done) {
     const authentication = {
       email: 'admin@fabatechnology.com',
       password: 'admin123'
@@ -21,32 +21,40 @@ describe('/POST post new job', () => {
       .send(authentication)
       .end((err, response) => {
         response.should.have.status(200);
-        const accessToken = response.body.token;
-        // After getting token finished, we set Authorization field in Header of request to create a new job
-        const job = {
-          title: 'Dev1',
-          salaryrange: '$450-$700',
-          description: 'Work with customers and dev team',
-          tags: ['English', 'Communicate', 'Technical knowledge'],
-          company: 'Faba',
-          logoURL: 'https://unsplash.com/photos/g2E2NQ5SWSU'
-        };
-        chai
-          .request(app())
-          .post('/api/job')
-          .set('Authorization', 'Bearer ' + accessToken)
-          .send(job)
-          .end((err, response) => {
-            response.should.have.status(201);
-            // Response.body.should.be.a('json');
-            response.body.should.have
-              .property('message')
-              .eql('Create the job successful!');
-            response.body.job.should.have.property('id');
-            response.body.job.should.have.property('title').eql(job.title);
-            response.body.job.should.have.property('tags').eql(job.tags);
-          });
+        accessToken = response.body.token;
         done();
       });
+  });
+  describe('/POST Post a new job', () => {
+    it('POST /api/jobs', (done) => {
+      const jobParameters = {
+        title: 'Dev187',
+        salaryRange: '$450-$700',
+        description: 'Work with customers and dev team',
+        tags: ['English', 'Communicate', 'Technical knowledge'],
+        company: 'Faba',
+        logoURL:
+          'https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+      };
+      chai
+        .request(app())
+        .post('/api/jobs')
+        .set('Authorization', 'Bearer ' + accessToken)
+        .send(jobParameters)
+        .end((err, response) => {
+          response.should.have.status(201);
+          response.body.should.have
+            .property('message')
+            .eql('Create the job successful!');
+          response.body.job.should.have.property('id');
+          response.body.job.should.have
+            .property('title')
+            .eql(jobParameters.title);
+          response.body.job.should.have
+            .property('tags')
+            .eql(jobParameters.tags);
+          done();
+        });
+    });
   });
 });
