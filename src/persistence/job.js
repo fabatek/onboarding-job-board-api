4c5b45ffaea8b7a2dee206f56c9844b4d7c82ad7
shinbe69 <rantarou@live.com>
2020-10-23 15:17:26 +0700

    T03 - Feature: Admin can update a job

--- src/persistence/job.js
+++ src/persistence/job.js
@@ -12,7 +12,7 @@ module.exports = {
       }, ${jobParameters.description}, current_date, ${jobParameters.tags}, ${
         jobParameters.company
       }, ${jobParameters.logoURL})
-        RETURNING id, title, tags;
+        RETURNING id, title, salary_range, description, create_at, tags, company, logo_url ;
       `);
       return rows[0];
     } catch (error) {
@@ -23,11 +23,52 @@ module.exports = {
       throw error;
     }
   },
-
   async find(id) {
     const {rows} = await db.query(sql`
     SELECT * FROM jobs WHERE id=${id} LIMIT 1;
     `);
     return rows[0];
+  },
+
+  async update(isJobExist, jobParameters) {
+    try {
+      let newTitle = jobParameters.title;
+      let newSalaryRange = jobParameters.salaryRange;
+      let newDescription = jobParameters.description;
+      let newTags = jobParameters.tags;
+      let newCompany = jobParameters.company;
+      let newLogoURL = jobParameters.logoURL;
+      if (newTitle.trim() === '') {
+        newTitle = isJobExist.title;
+      }
+
+      if (newSalaryRange.trim() === '') {
+        newSalaryRange = isJobExist.salaryRange;
+      }
+
+      if (newDescription.trim() === '') {
+        newDescription = isJobExist.description;
+      }
+
+      if (newTags.length === 0) {
+        newTags = isJobExist.tags;
+      }
+
+      if (newCompany.trim() === '') {
+        newCompany = isJobExist.company;
+      }
+
+      if (newLogoURL.trim() === '') {
+        newLogoURL = isJobExist.logoURL;
+      }
+
+      const {rows} = await db.query(sql`
+        UPDATE jobs SET title = ${newTitle}, salary_range = ${newSalaryRange}, description = ${newDescription}, tags = ${newTags}, company = ${newCompany}, logo_url = ${newLogoURL} WHERE id = ${isJobExist.id}
+          RETURNING id, title, salary_range, description, create_at, tags, company, logo_url ;
+        `);
+      return rows;
+    } catch (error) {
+      throw error;
+    }
   }
 };
