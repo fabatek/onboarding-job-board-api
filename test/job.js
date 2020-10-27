227a9dcf54e65c763344aa4a845cf34f343485d5
shinbe69 <37283758+shinbe69@users.noreply.github.com>
2020-10-27 14:07:41 +0700

    Update job.js

--- test/job.js
+++ test/job.js
@@ -59,31 +59,4 @@ describe('/POST Post a new job', () => {
         });
     });
   });
-
-  describe('/PUT Update the job has just been create', () => {
-    it('PUT /api/jobs', (done) => {
-      const jobParameters = {
-        id,
-        title: 'ChangeTitleToThisLine',
-        salaryRange: '$450-$700',
-        description: 'Work with customers and dev team',
-        tags: ['English', 'Communicate', 'Technical knowledge'],
-        company: 'Faba',
-        logoURL:
-          'https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
-      };
-      chai
-        .request(app())
-        .put('/api/jobs')
-        .set('Authorization', 'Bearer ' + accessToken)
-        .send(jobParameters)
-        .end((err, response) => {
-          response.should.have.status(200);
-          response.body.should.have
-            .property('message')
-            .eql('Update the job successful!');
-          done();
-        });
-    });
-  });
 });
