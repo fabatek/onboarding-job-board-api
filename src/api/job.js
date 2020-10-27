7e33c8787050b75966d8abe14d334017426989df
shinbe69 <rantarou@live.com>
2020-10-27 14:22:50 +0700

    Code update job api

--- src/api/job.js
+++ src/api/job.js
@@ -50,4 +50,34 @@ router.post('/', async (request, response) => {
     response.status(500).json();
   }
 });
+
+router.put('/', async (request, response) => {
+  try {
+    const {id} = request.body;
+    // eslint-disable-next-line unicorn/no-fn-reference-in-iterator
+    const isJobExist = await Job.find(id);
+    if (!isJobExist) {
+      return response
+        .status(400)
+        .json({message: 'There is no job with this id!'});
+    }
+
+    const jobParameters = request.body;
+    const job = await Job.update(isJobExist, jobParameters);
+    if (!job) {
+      return response.status(400).json({message: 'Update job failed!'});
+    }
+
+    return response.status(200).json({
+      message: 'Update the job successful!',
+      job
+    });
+  } catch (error) {
+    console.error(
+      `updateJob({ title: ${request.body.title} }) >> Error: ${error.stack}`
+    );
+
+    response.status(500).json();
+  }
+});
 module.exports = router;
--- test/job.js
+++ test/job.js
@@ -47,14 +47,34 @@ describe('/POST Post a new job', () => {
           response.body.should.have
             .property('message')
             .eql('Create the job successful!');
-          response.body.job.should.have.property('id');
-          response.body.job.should.have
-            .property('title')
-            .eql(jobParameters.title);
-          response.body.job.should.have
-            .property('tags')
-            .eql(jobParameters.tags);
           id = response.body.job.id;
+          done();
+        });
+    });
+  });
+
+  describe('/PUT Update the job has just been create', () => {
+    it('PUT /api/jobs', (done) => {
+      const jobParameters = {
+        id,
+        title: 'ChangeTitleToThisLine',
+        salaryRange: '$450-$700',
+        description: 'Work with customers and dev team',
+        tags: ['English', 'Communicate', 'Technical knowledge'],
+        company: 'Faba',
+        logoURL:
+          'https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+      };
+      chai
+        .request(app())
+        .put('/api/jobs')
+        .set('Authorization', 'Bearer ' + accessToken)
+        .send(jobParameters)
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.have
+            .property('message')
+            .eql('Update the job successful!');
           done();
         });
     });
