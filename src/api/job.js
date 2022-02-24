ad71a6fbe63f6721746a242d500be3d56a455aa9
truongtrungkien0999 <truongtrungkien0999@gmail.com>
2022-02-24 10:24:24 +0700

    update t02 admin can create a new job

---
+++ src/api/job.js
@@ -0,0 +1,29 @@
+const { Router } = require('express');
+const Job = require('../persistence/jobs');
+
+const router = new Router();
+
+router.post('/', async(request, response) => {
+    try {
+        const infoJob = request.body;
+
+        if (!infoJob.title) {
+            return response.status(400).json({ message: 'Job title is required.' });
+        }
+
+        const job = await Job.create(infoJob);
+        if (!job) {
+            return response.status(400).json({ message: 'Job already exists.' });
+        }
+
+        return response.status(201).json(job);
+    } catch (error) {
+        console.error(
+            `createJob({ title: ${request.body.title} }) >> Error: ${error.stack}`
+        );
+
+        response.status(500).json();
+    }
+});
+
+module.exports = router;
