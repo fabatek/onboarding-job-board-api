951c408e7d1d1e2daac84d88b8e5794915ed2a33
truongtrungkien0999 <truongtrungkien0999@gmail.com>
2022-02-28 14:30:25 +0700

    update t03 admin can update a job

--- src/persistence/jobs.js
+++ src/persistence/jobs.js
@@ -18,5 +18,24 @@ module.exports = {
             if (error.constraint === 'jobs_title_key') return null;
             throw error;
         }
+    },
+
+    async update(jobId, job) {
+        const { title, salary_range, description, tags, company_name, company_logo } = job;
+
+        try {
+            const {rows} = await db.query(sql`
+            UPDATE jobs
+            SET title = ${title}, salary_range = ${salary_range}, description = ${description}, 
+            tags = ${tags}, company_name = ${company_name}, company_logo = ${company_logo}
+            WHERE id = ${jobId}
+            RETURNING *;
+            `);
+            const [jobUpdated] = rows;
+            return jobUpdated;
+        } catch (error) {
+            if (error.constraint === 'jobs_title_key') return null;
+            throw error;
+        }
     }
 }
